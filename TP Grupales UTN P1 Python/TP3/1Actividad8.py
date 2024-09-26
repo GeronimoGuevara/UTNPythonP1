8807f9ec1a4625b81b7aa9a03405d60068cd13cd
GeronimoGuevara <geronimoguevaramansuino@gmail.com>
2024-09-25 23:08:54 -0300

    Agregar archivo 8 correcto

--- TP Grupales UTN P1 Python/TP3/1Actividad8.py
+++ TP Grupales UTN P1 Python/TP3/1Actividad8.py
@@ -5,18 +5,20 @@ Pista:
 Utiliza un diccionario o un conjunto (set) para hacer el seguimiento 
 de los elementos.
 """
+
+def encontrar_elementos_repetidos(lista):
+    vistos = set()
+    repetidos = set()
+
+    for elemento in lista:
+        if elemento in vistos:
+            repetidos.add(elemento)
+        else:
+            vistos.add(elemento)
+
+    return list(repetidos)
+
 numeros = input("Ingresa una lista de números separados por espacio: ")
 lista_numeros = list(map(int, numeros.split()))
-
-print(lista_numeros)
-
-vistos = set()
-repetidos = set()
-
-for elemento in lista_numeros:
-    if elemento in vistos:
-        repetidos.add(elemento)
-    else:
-        vistos.add(elemento)
-
-print(repetidos)
+repetidos = encontrar_elementos_repetidos(lista_numeros)
+print("Elementos repetidos:", repetidos)
