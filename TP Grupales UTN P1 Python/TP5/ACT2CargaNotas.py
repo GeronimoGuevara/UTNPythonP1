ba46cb5eff6c9d40ad20b2277a17836be79b3446
GeroGuevara <geronimoguevaramansuino@gmail.com>
2024-11-01 09:28:38 -0300

    Correcciones

--- TP Grupales UTN P1 Python/TP5/ACT2CargaNotas.py
+++ TP Grupales UTN P1 Python/TP5/ACT2CargaNotas.py
@@ -7,20 +7,17 @@ class CargaNotas:
         alumnos = []
         
         while True:
-            # Crear una instancia de Alumno
             print("INGRESE DATOS DEL ALUMNO")
             nombre_completo = input("Ingrese nombre completo: ")
             legajo = int(input("Ingrese legajo: "))
             alumno = Alumno(nombre_completo, legajo)
             
-            # Cargar Notas para el Alumno
             while True:
                 print("INGRESE DATOS DE LA NOTA")
                 catedra = input("Ingrese nombre de la cátedra: ")
                 nota_examen = float(input("Ingrese la nota del examen: "))
                 nota = Nota(catedra, nota_examen)
                 
-                # Agregar la nota al alumno
                 alumno.agregar_nota(nota)
                 
                 salir_notas = input("¿Desea salir de la carga de notas? (s/n): ").lower()
@@ -29,14 +26,12 @@ class CargaNotas:
                 elif salir_notas == 's' and len(alumno.notas) == 0:
                     print("Debe ingresar al menos una nota.")
             
-            # Agregar el alumno a la lista de alumnos
             alumnos.append(alumno)
             
             salir_alumno = input("¿Desea salir de la carga de alumnos? (s/n): ").lower()
             if salir_alumno == 's':
                 break
         
-        # Mostrar información de los alumnos y sus notas
         for alumno in alumnos:
             print("\nDatos del Alumno:")
             print(f"Nombre: {alumno.nombre_completo}")
@@ -46,6 +41,5 @@ class CargaNotas:
                 print(f"Cátedra: {nota.catedra}, Nota: {nota.nota_examen}")
             print(f"Promedio del Alumno: {alumno.calcular_promedio()}")
 
-# Ejecutar el programa
 if __name__ == "__main__":
     CargaNotas.main()
